af98a4f2f27a006bbc748e233d97a73dc62db560
Hayden McAfee <hayden@outlook.com>
2016-03-16 16:24:07 -0700

    My first ball game! Just a ball that rolls around a stage.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float MovementForce;
+    private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    // Called every frame before physics calculations
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        var movementVector = new Vector3(moveHorizontal, 0, moveVertical);
+        rigidBody.AddForce(movementVector * MovementForce);
+    }
+}
